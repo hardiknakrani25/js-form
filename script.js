1172d3d6e960c2031b76f3d9ad4b3d8b08f5b0ce
Hardik Nakrani <hdnakarani@gmail.com>
2019-01-20 17:12:33 +0530

    Registration form

---
+++ script.js
@@ -0,0 +1,28 @@
+function getParams(){
+                var idx = document.URL.indexOf('?');
+                var params = new Array();
+                if (idx != -1) {
+                    var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
+                    for (var i=0; i<pairs.length; i++){
+                        nameVal = pairs[i].split('=');
+                        params[nameVal[0]] = nameVal[1];
+                    }
+                }
+                return params;
+            }
+            params = getParams();
+            firstname = unescape(params["firstname"]);
+            lastname = unescape(params["lastname"]);
+            age = unescape(params["age"]);
+            phone = unescape(params["phone"]);
+            email = unescape(params["email"]);
+            address = unescape(params["address"]);
+        
+            document.write("<h1>Registration Details</h1><br>")
+
+            document.write("firstname = " + firstname + "<br>");
+            document.write("lastname = " + lastname + "<br>");
+            document.write("age = " + age + "<br>");
+            document.write("phone = " + phone + "<br>");
+            document.write("email = " + email + "<br>");
+            document.write("address = " + address + "<br>");
